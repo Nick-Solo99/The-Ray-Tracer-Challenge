1d02f2ebba4d587d7bc97ab0c1113bc7d3592d8d
Nicholas Solomon <nicholassolomon99@outlook.com>
2026-05-11 16:41:37 -0700

    Identifying Hits

--- src/intersections/Intersection.cpp
+++ src/intersections/Intersection.cpp
@@ -3,9 +3,23 @@
 //
 
 #include "Intersection.h"
+#include <span>
+#include <algorithm>
 
 namespace rtc::intersections {
 
+    std::vector<Intersection> intersections(const std::initializer_list<Intersection> list) {
+        std::vector<Intersection> result{list};
+        std::sort(result.begin(), result.end());
+        return result;
+    }
 
-
+    std::optional<Intersection> hit(const std::span<const Intersection> intersections) {
+        for (const auto& intersection : intersections) {
+            if (intersection.t >= 0) {
+                return intersection;
+            }
+        }
+        return std::nullopt;
+    }
 }
