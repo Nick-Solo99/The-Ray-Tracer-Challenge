ce5b78b1f779cd758b9d504e74927aeee65a8699
Nicholas Solomon <nicholassolomon99@outlook.com>
2026-05-22 11:47:40 -0700

    Capped Cylinders

--- src/shapes/cylinders/Cylinder.h
+++ src/shapes/cylinders/Cylinder.h
@@ -9,9 +9,12 @@
 
 namespace rtc::shapes::cylinders {
     class Cylinder final : public Shape {
+        static bool check_cap(const Ray& ray, const float& t) ;
+        void intersect_caps(const Ray& ray, std::vector<intersections::Intersection>& xs) const;
     public:
         float minimum = -INFINITY;
         float maximum = INFINITY;
+        bool closed = false;
         [[nodiscard]] std::vector<intersections::Intersection> intersect(const Ray& ray) const override;
         [[nodiscard]] Vector normal_at(const Point& point) const override;
     };
--- tests/cylinders_tests.cpp
+++ tests/cylinders_tests.cpp
@@ -126,3 +126,59 @@ SCENARIO("Intersecting a constrained cylinder") {
         }
     }
 }
+
+SCENARIO("The default closed value for a cylinder") {
+    GIVEN("cyl <- cylinder()") {
+        const Cylinder cyl{};
+        THEN("cyl.closed = false") {
+            REQUIRE(!cyl.closed);
+        }
+    }
+}
+
+SCENARIO("Intersecting the caps of a closed cylinder") {
+    auto [origin, direction, count] = GENERATE(table<Point, Vector, float>({
+        {point(0, 3, 0), vector(0, -1, 0), 2},
+        {point(0, 3, -2), vector(0, -1, 2), 2},
+        {point(0, 4, -2), vector(0, -1, 1), 2},
+        {point(0, 0, -2), vector(0, 1, 2), 2},
+        {point(0, -1, -2), vector(0, 1, 1), 2}
+    }));
+    GIVEN("cyl <- cylinder(), cyl.minimum = 1, cyl.maximum = 2, cyl.closed = true, dir <- normalize(<direction>), r <- ray(<origin>, dir)") {
+        Cylinder cyl{};
+        cyl.minimum = 1;
+        cyl.maximum = 2;
+        cyl.closed = true;
+        const Vector dir = normalize(direction);
+        const Ray r{origin, dir};
+        WHEN("xs <- cyl.intersect(r)") {
+            const auto xs = cyl.intersect(r);
+            THEN("xs.count == <count>") {
+                REQUIRE(xs.size() == count);
+            }
+        }
+    }
+}
+
+SCENARIO("The normal vector on a cylinders end caps") {
+    auto [origin, normal] = GENERATE(table<Point, Vector>({
+        {point(0, 1, 0), vector(0, -1, 0)},
+        {point(0.5, 1, 0), vector(0, -1, 0)},
+        {point(0, 1, 0.5), vector(0, -1, 0)},
+        {point(0, 2, 0), vector(0, 1, 0)},
+        {point(0.5, 2, 0), vector(0, 1, 0)},
+        {point(0, 2, 0.5), vector(0, 1, 0)},
+    }));
+    GIVEN("cyl <- cylinder(), cyl.minimum <- 1, cyl.maximum <- 2, cyl.closed <- true") {
+        Cylinder cyl{};
+        cyl.minimum = 1;
+        cyl.maximum = 2;
+        cyl.closed = true;
+        WHEN("n <- cyl.normal_at(point(" << origin.x << ", " << origin.y << ", " << origin.z << "))") {
+            const auto n = cyl.normal_at(origin);
+            THEN("n = vector(" << normal.x << ", " << normal.y << ", " << normal.z << ")") {
+                REQUIRE(n == normal);
+            }
+        }
+    }
+}
